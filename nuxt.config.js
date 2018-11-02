2075160746052c36222d5b6d73ac6c978dff4d71
Max <tnalsl1984@naver.com>
2018-11-02 17:06:43 +0900

    [ set ] - [ 디렉토리 설정 ] [    프로젝트 디렉토리 설정    /src 로 설정 ]

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,30 +1,39 @@
 module.exports = {
+  srcDir: '/src',
+
   /*
   ** Headers of the page
   */
   head: {
     title: 'membership-front-core',
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'MemberShip Front Core Project' }
+      {charset: 'utf-8'},
+      {name: 'viewport', content: 'width=device-width, initial-scale=1'},
+      {
+        hid: 'description',
+        name: 'description',
+        content: 'MemberShip Front Core Project'
+      }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      {rel: 'icon', type: 'image/x-icon', href: '/favicon.ico'}
     ]
   },
+
   /*
   ** Customize the progress bar color
   */
-  loading: { color: '#3B8070' },
+  loading: {color: '#3B8070'},
+
   /*
   ** Build configuration
   */
   build: {
+
     /*
     ** Run ESLint on save
     */
-    extend (config, { isDev, isClient }) {
+    extend(config, {isDev, isClient}) {
       if (isDev && isClient) {
         config.module.rules.push({
           enforce: 'pre',
